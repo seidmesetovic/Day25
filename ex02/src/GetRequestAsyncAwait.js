257d5fc9f4d4a509ec3411ac1f2eaf4158b7c768
seidmesetovic <seid.mesetovic@gmail.com>
2021-04-02 01:12:39 +0200

    ex02 gotov

---
+++ ex02/src/GetRequestAsyncAwait.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class GetRequestAsyncAwait extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { totalReactPackages: null };
+
+    }
+    async componentDidMount() {
+        const response = await fetch('https://api.npms.io/v2/search?q=react');
+        const data = await response.json();
+        this.setState({ totalReactPackages: data.total });
+
+    }
+    render() {
+        const { totalReactPackages } = this.state;
+        return (
+            <div className="card-text-center m-3">
+                <h5 className="card-header">Get request with Async/Await</h5>
+                <div className="card-body">Total packeges: {totalReactPackages}</div>
+            </div>
+        );
+    }
+
+}
+export default GetRequestAsyncAwait;
