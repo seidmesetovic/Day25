8681dfd5a959c0fdc18612f162ebe736d2253521
seidmesetovic <seid.mesetovic@gmail.com>
2021-04-02 00:49:59 +0200

    ex01 gotov

---
+++ ex01/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import GetRequest from './GetRequest';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequest />
+    </div>
+  );
+}
+
+export default App;
