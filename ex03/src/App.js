9b71b3729ca7030a031ba1b2a730448ca30104db
seidmesetovic <seid.mesetovic@gmail.com>
2021-04-02 01:32:27 +0200

    ex03 gotov

---
+++ ex03/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import GetRequestErrorHandling from './GetRequestErrorHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
