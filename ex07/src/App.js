9445ba465ae67076e3e15aed2419bc04aeae4594
seidmesetovic <seid.mesetovic@gmail.com>
2021-04-02 10:53:20 +0200

    ex07 gotov

---
+++ ex07/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PostRequestErrorHandling from './PostRequestErrorHandling';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
