7b769a6d1315bda73379730935ca8bb7b158f571
seidmesetovic <seid.mesetovic@gmail.com>
2021-04-02 02:47:48 +0200

    ex04 gotov

---
+++ ex04/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GetRequestSetHeaders from './GetRequestSetHeaders';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+
+export default App;
